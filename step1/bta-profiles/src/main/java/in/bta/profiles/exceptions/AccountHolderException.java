5181b6bf2d196e803e7a4bbd98ff30070ce3fe35
Vamsy Kiran A <a.vamc.it@gmail.com>
2024-08-14 18:21:27 +0530

    step1 and step2 OpenFeign, Eureka, Aggregator Pattern, LoadBalancing

---
+++ step1/bta-profiles/src/main/java/in/bta/profiles/exceptions/AccountHolderException.java
@@ -0,0 +1,14 @@
+package in.bta.profiles.exceptions;
+
+public class AccountHolderException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AccountHolderException(String message) {
+		super(message);
+	}
+	
+}
---
+++ step1/bta-statement/src/main/java/in/bta/statement/exceptions/StatementException.java
@@ -0,0 +1,14 @@
+package in.bta.statement.exceptions;
+
+public class StatementException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StatementException(String arg0) {
+		super(arg0);
+	}
+
+}
